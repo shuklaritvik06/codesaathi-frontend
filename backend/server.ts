0c72f8720245f9f8af5b772be71d185bc3046f5a
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-05-19 01:46:32 +0530

    update

--- backend/server.ts
+++ backend/server.ts
@@ -5,96 +5,99 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 let clients: any = {};
+
 type Users = {
-  [username: string]: string;
+	[username: string]: string;
 };
+
 function getAllConnectedClients(roomId: string) {
-  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
-    (socketId) => {
-      return {
-        socketId,
-        username: clients[socketId]
-      };
-    }
-  );
+	return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+		(socketId) => {
+			return {
+				socketId,
+				username: clients[socketId],
+			};
+		}
+	);
 }
+
 io.sockets.on("connection", (socket) => {
-  socket.on("join", ({ roomId, username }) => {
-    if (clients[roomId] === undefined) {
-      clients[roomId] = [
-        {
-          [username]: socket.id
-        }
-      ];
-    } else {
-      let new_clients = clients[roomId].filter((user: Array<Users>) => {
-        return Object.keys(user)[0] !== username;
-      });
-      clients[roomId] = [
-        ...new_clients,
-        {
-          [username]: socket.id
-        }
-      ];
-    }
-    socket.join(roomId);
-    const all_clients = getAllConnectedClients(roomId);
-    all_clients.forEach(({ socketId }) => {
-      io.to(socketId).emit("newjoined", {
-        clients,
-        username,
-        socketId: socket.id
-      });
-    });
-  });
+	socket.on("join", ({ roomId, username }) => {
+		if (clients[roomId] === undefined) {
+			clients[roomId] = [
+				{
+					[username]: socket.id,
+				},
+			];
+		} else {
+			let new_clients = clients[roomId].filter((user: Array<Users>) => {
+				return Object.keys(user)[0] !== username;
+			});
+			clients[roomId] = [
+				...new_clients,
+				{
+					[username]: socket.id,
+				},
+			];
+		}
+		socket.join(roomId);
+		const all_clients = getAllConnectedClients(roomId);
+		all_clients.forEach(({ socketId }) => {
+			io.to(socketId).emit("newjoined", {
+				clients,
+				username,
+				socketId: socket.id,
+			});
+		});
+	});
 
-  socket.on("chat", ({ room, username, message }) => {
-    Object.keys(clients).forEach((roomID) => {
-      if (roomID === room) {
-        clients[room].forEach((client: Object) => {
-          io.to(Object.values(client)).emit("newchat", {
-            msg: message,
-            username,
-            room
-          });
-        });
-      }
-    });
-  });
+	socket.on("chat", ({ room, username, message }) => {
+		Object.keys(clients).forEach((roomID) => {
+			if (roomID === room) {
+				clients[room].forEach((client: Object) => {
+					io.to(Object.values(client)).emit("newchat", {
+						msg: message,
+						username,
+						room,
+					});
+				});
+			}
+		});
+	});
 
-  socket.on("newcode", ({ roomId, code }) => {
-    Object.keys(clients).forEach((room) => {
-      if (room === roomId) {
-        clients[room].forEach((client: Object) => {
-          io.to(Object.values(client)).emit("codechange", {
-            code
-          });
-        });
-      }
-    });
-  });
+	socket.on("newcode", ({ roomId, code }) => {
+		Object.keys(clients).forEach((room) => {
+			if (room === roomId) {
+				clients[room].forEach((client: Object) => {
+					io.to(Object.values(client)).emit("codechange", {
+						code,
+					});
+				});
+			}
+		});
+	});
 
-  socket.on("disconnecting", () => {
-    const rooms = [...socket.rooms];
-    rooms.forEach((roomId) => {
-      socket.in(roomId).emit("disconnected", {
-        socketId: socket.id,
-        username: clients[socket.id]
-      });
-      socket.leave(roomId);
-      Object.keys(clients).forEach((room) => {
-        if (roomId === room) {
-          clients[room].forEach((client: Object) => {
-            if (Object.values(client)[0] === socket.id) {
-              delete clients[room][Object.keys(client)[0]];
-            }
-          });
-        }
-      });
-    });
-  });
+	socket.on("disconnecting", () => {
+		const rooms = [...socket.rooms];
+		rooms.forEach((roomId) => {
+			socket.in(roomId).emit("disconnected", {
+				socketId: socket.id,
+				username: clients[socket.id],
+			});
+			socket.leave(roomId);
+			Object.keys(clients).forEach((room) => {
+				if (roomId === room) {
+					clients[room].forEach((client: Object) => {
+						if (Object.values(client)[0] === socket.id) {
+							delete clients[room][Object.keys(client)[0]];
+						}
+					});
+				}
+			});
+		});
+	});
 });
 
 server.listen(5000, () => {
-  console.log("listening on *:5000");
+	console.log("listening on *:5000");
 });
